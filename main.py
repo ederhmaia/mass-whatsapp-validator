5299375a96bc96884e37c8e048798359bb263235
EDER JOSE DA MAIA FILHO <edermaiafilho@outlook.com>
2023-03-06 04:15:19 -0300

    3min stupid script

--- main.py
+++ main.py
@@ -1,66 +1,84 @@
-from webdriver_manager.chrome import ChromeDriverManager
+# selenium imports
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-import undetected_chromedriver as uc
-from time import sleep
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
+
+# cdm
+from webdriver_manager.chrome import ChromeDriverManager
+
+# commom imports
+from time import sleep
 import os
 
-filename = 'list.txt'
+# typing imports
+from typing import List
 
+# sugar imports
+from colorama import Fore, init
+
+# startups
+init(autoreset=True)
 os.environ['WDM_LOG_LEVEL'] = '0'
-number_list = []
 
-file = open(filename, "r")
-
-for line in file.readlines():
-    number_list.append(line.strip())
-
-options = Options()
-options.add_argument(r'--user-data-dir=C:\Users\ederm\AppData\Local\Google\Chrome\User Data')
-options.add_argument('--profile-directory=Profile 32')
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-service = Service(ChromeDriverManager().install())
-
+def get_current_hour_and_minute() -> str:
+    from datetime import datetime
+    return datetime.now().strftime("%H:%M")
 
 def write_to_file(filename: str, data: str):
     with open(filename, 'a') as file:
-       file.write(data)
-       file.write('\n')
+        file.write(f"{data}\n")
 
-driver = webdriver.Chrome(service=service, options=options)
-driver.get("https://web.whatsapp.com/")
-sleep(10)
+def main(phone_list: List[str]) -> None:
+    service = Service(ChromeDriverManager().install())
+    options = Options()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
+    # change the path to your chrome user data path
+    options.add_argument(r'--user-data-dir=C:\Users\ederm\AppData\Local\Google\Chrome\User Data')
+    # select the profile that has whatsapp web logged in (qrcode scanned)
+    options.add_argument('--profile-directory=Profile 32')
 
-for number in number_list:
-    driver.execute_script(f"link = document.createElement('a');")
-    driver.execute_script(f"link.href = 'https://wa.me/55{number}?text=Ola';")
-    driver.execute_script(f'document.body.appendChild(link);')
-    driver.execute_script(f'link.click();')
-    sleep(3)
-
-    try:
-        validator = driver.execute_script(f'return document.querySelector("#app > div > span:nth-child(2) > div > span > div > div > div > div > div > div.f8jlpxt4.iuhl9who").innerText')
-
-        if validator == 'O número de telefone compartilhado através de url é inválido.':
-            print(f"{number} é um whatsapp invalido")
-            write_to_file('invalid.txt', number)        
-        else:
-            raise Exception('Whatsapp valido')
-    except:
-        print(f"{number} é um whatsapp valido")
-        write_to_file('valid.txt', number)
-
-    finally:
-        driver.execute_script(f'window.location.reload();')
-        sleep(5)
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get("https://web.whatsapp.com/")
+    sleep(15)
 
 
-driver.quit()
+    for number in phone_list:
+        driver.execute_script(f"link = document.createElement('a');")
+        driver.execute_script(f"link.href = 'https://wa.me/55{number}?text=Ola';")
+        driver.execute_script(f'document.body.appendChild(link);')
+        driver.execute_script(f'link.click();')
+        sleep(4)
+
+        try:
+            error_flag = driver.execute_script(f'return document.querySelector("#app > div > span:nth-child(2) > div > span > div > div > div > div > div > div.f8jlpxt4.iuhl9who").innerText')
+
+            if error_flag == 'O número de telefone compartilhado através de url é inválido.':
+                print(f"{get_current_hour_and_minute()} {number}{Fore.RED} é um whatsapp invalido")
+                write_to_file('invalids.txt', number)   
+                driver.execute_script(f"document.querySelector('#app > div > span:nth-child(2) > div > span > div > div > div > div > div > div.f8jlpxt4.iuhl9who').remove();")     
+            else:
+                raise Exception()
+        except:
+            print(f"{get_current_hour_and_minute()} {number}{Fore.GREEN} é um whatsapp valido")
+            write_to_file('valids.txt', number)
+
+    driver.close()
+
+
+if __name__ == '__main__':
+    print(f"[@]{Fore.YELLOW} Digite o nome do arquivo.")
+    filename: str = input(">>>")
+
+    if not filename.endswith('.txt'):
+        filename += '.txt'
+    
+    stdin_list: List[str] = []
+    
+    for line in open(filename, "r").readlines():
+        stdin_list.append(line.strip())
+    
+    main(phone_list=stdin_list)
